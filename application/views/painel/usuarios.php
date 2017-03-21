89e7afc7e1174867fa84a08addecd3852a711fcb
Leiviton Carlos <leivitoncs@gmail.com>
2017-03-21 11:34:18 -0300

    base_url

--- application/views/painel/usuarios.php
+++ application/views/painel/usuarios.php
@@ -95,7 +95,7 @@ switch ($tela) {
                 <div class="card-content">    
                   <div class="form-group">
                     <div class="col-md-6 col-md-offset-4 text-center">                                      
-                      <a class="btn btn-info btn-lg " href="https://direta.net.br/app/public">Ir para o login</a>
+                      <a class="btn btn-primary btn-lg " href="https://direta.net.br/app/public">Ir para o login</a>
                     </div>
                   </div>
                 </div>
