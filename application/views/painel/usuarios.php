76f7bf13cf77654016b1ad73522e67df0f115fe5
Leiviton Carlos <leivitoncs@gmail.com>
2017-03-21 11:50:25 -0300

    base_url

--- application/views/painel/usuarios.php
+++ application/views/painel/usuarios.php
@@ -54,13 +54,11 @@ switch ($tela) {
                   <h4 class="card-title">Primeiro Acesso</h4>
                 </div>
                 <div class="card-content">
-                 <div class="input-group">
-                  <div class="col-md-4"></div>
+                 <div class="input-group" style="margin-left: 110%;">                  
                   <span class="input-group-addon">
                       <i class="material-icons">face</i>
                   </span>
-                  <div class="form-group label-floating is-empty">
-                    
+                  <div class="form-group label-floating is-empty">                    
                     <label class="col-md-4 control-label">Seu CPF</label>
                     <div class="col-md-6">
                       <input type="text" class="form-control" name="cpf" required="true">
