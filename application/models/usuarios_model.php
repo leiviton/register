4144177835e36377d09f1b9935ff812f1047c74c
Leiviton Carlos <leivitoncs@gmail.com>
2017-03-21 16:16:04 -0300

    base_url

--- application/models/usuarios_model.php
+++ application/models/usuarios_model.php
@@ -135,7 +135,7 @@ class Usuarios_model extends CI_Model
 			return FALSE;
 		}		
 	}
-	public function grava_chave($chave='',$email='',$cpf=NULL)
+	public function grava_chave($chave='',$email='',$cpf='')
 	{
 		if ($this->db->insert('token_validator', array('chave'=>$chave,'email'=>$email,'cpf'=>$cpf)) {
 			return TRUE;	
