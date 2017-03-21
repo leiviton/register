1191ab901ee7da2fe5b6a95db6461f12577df96c
Leiviton Carlos <leivitoncs@gmail.com>
2017-03-21 15:51:23 -0300

    base_url

--- application/config/config.php
+++ application/config/config.php
@@ -22,9 +22,9 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-//$config['base_url'] = 'https://direta.net.br/register/';
+$config['base_url'] = 'https://direta.net.br/register/';
 
-$config['base_url'] = 'http://localhost/sistema/';
+//$config['base_url'] = 'http://localhost/sistema/';
 
 /*
 |--------------------------------------------------------------------------
